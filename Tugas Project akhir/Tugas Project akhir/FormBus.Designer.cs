eed6e856be80d358aab29e63bef1731468a9065c
AndreasResa <142899031+AndreasResa@users.noreply.github.com>
2023-11-15 16:23:24 +0800

    Add files via upload

---
+++ Tugas Project akhir/Tugas Project akhir/FormBus.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Tugas_Project_akhir
+{
+    partial class FormBus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtnim = new System.Windows.Forms.TextBox();
+            this.txtbus = new System.Windows.Forms.TextBox();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.btncari = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.txtket = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(239, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(306, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DATA BUS FILKOM DAY ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Bus";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(20, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NIM";
+            // 
+            // txtnim
+            // 
+            this.txtnim.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.txtnim.Location = new System.Drawing.Point(157, 57);
+            this.txtnim.Name = "txtnim";
+            this.txtnim.Size = new System.Drawing.Size(263, 22);
+            this.txtnim.TabIndex = 4;
+            // 
+            // txtbus
+            // 
+            this.txtbus.Location = new System.Drawing.Point(157, 135);
+            this.txtbus.Name = "txtbus";
+            this.txtbus.Size = new System.Drawing.Size(263, 22);
+            this.txtbus.TabIndex = 5;
+            // 
+            // txtnama
+            // 
+            this.txtnama.Location = new System.Drawing.Point(157, 95);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(263, 22);
+            this.txtnama.TabIndex = 6;
+            // 
+            // btncari
+            // 
+            this.btncari.Location = new System.Drawing.Point(503, 70);
+            this.btncari.Name = "btncari";
+            this.btncari.Size = new System.Drawing.Size(75, 23);
+            this.btncari.TabIndex = 7;
+            this.btncari.Text = "Cari";
+            this.btncari.UseVisualStyleBackColor = true;
+            this.btncari.Click += new System.EventHandler(this.btncari_Click_1);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(558, 120);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 8;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click_1);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(619, 70);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
+            this.btncancel.TabIndex = 9;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 220);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(741, 197);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.Location = new System.Drawing.Point(22, 173);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(124, 25);
+            this.lbl5.TabIndex = 12;
+            this.lbl5.Text = "Keterangan";
+            // 
+            // txtket
+            // 
+            this.txtket.Location = new System.Drawing.Point(157, 176);
+            this.txtket.Name = "txtket";
+            this.txtket.Size = new System.Drawing.Size(263, 22);
+            this.txtket.TabIndex = 13;
+            // 
+            // FormBus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(765, 429);
+            this.Controls.Add(this.txtket);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btncari);
+            this.Controls.Add(this.txtnama);
+            this.Controls.Add(this.txtbus);
+            this.Controls.Add(this.txtnim);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "FormBus";
+            this.Text = "FormBus";
+            this.Load += new System.EventHandler(this.FormBus_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtnim;
+        private System.Windows.Forms.TextBox txtbus;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.Button btncari;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.TextBox txtket;
+    }
+}
+
---
+++ Tugas Project akhir/Tugas Project akhir/daftarbus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormBus
+{
+    public partial class daftarbus : Form
+    {
+        public daftarbus()
+        {
+            InitializeComponent();
+        }
+    }
+}
